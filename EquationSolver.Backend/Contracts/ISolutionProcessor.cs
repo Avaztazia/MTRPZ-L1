91d0985560c72e42345be58eac6a99daedecb9e2
Avaztazia <71588076+Avaztazia@users.noreply.github.com>
2022-02-19 11:21:24 +0200

    Add solver contracts

---
+++ EquationSolver.Backend/Contracts/ISolutionProcessor.cs
@@ -0,0 +1,8 @@
+﻿using EquationSolver.Backend.Models;
+
+namespace EquationSolver.Backend.Contracts;
+
+public interface ISolutionProcessor
+{
+    SolverResponse SolveEquation(EquationData input);
+}
---
+++ EquationSolver.Backend/Models/SolverResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace EquationSolver.Backend.Models;
+
+public class SolverResponse
+{
+    public int RootsAmount { get; set; }
+    public List<double> Roots { get; set; }
+
+    public SolverResponse()
+    {
+        Roots = new List<double>(0);
+    }
+}
